d8c3a72593b5d85471261f6e6ecfc63992fcc3e0
Mike Crandall <michaelgcrandall@gmail.com>
2024-09-17 15:22:17 -0600

    added build num

--- moog/HomeViewController.swift
+++ moog/HomeViewController.swift
@@ -15,6 +15,7 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var waveformButton: UIButton!
     @IBOutlet weak var multiviewButton: UIButton!
     @IBOutlet weak var testButton: UIButton!
+    @IBOutlet weak var buildLabel: UILabel!
 
     
     override func viewDidLoad() {
@@ -33,10 +34,19 @@ class HomeViewController: UIViewController {
         waveformButton.titleLabel?.font = UIFont.systemFont(ofSize: 30)
         multiviewButton.titleLabel?.font = UIFont.systemFont(ofSize: 30)
 
+        buildLabel.font = UIFont.systemFont(ofSize: 15)
+        buildLabel.textColor = .black
+
         thereScopeButton.isHidden = false
         waveformButton.isHidden = false
         multiviewButton.isHidden = false
         testButton.isHidden = true
+        
+        // get the build number:
+        if let buildNumber = Bundle.main.infoDictionary?["CFBundleVersion"] as? String {
+            buildLabel.text = "(build: \(buildNumber))"
+        }
+
 
     }
     
