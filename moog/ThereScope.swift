7426aa405b6b1457eb9aa47c8359a66521d5480a
Mike Crandall <michaelgcrandall@gmail.com>
2024-10-25 17:32:33 -0600

    prepare for background mode

--- moog/ThereScope.swift
+++ moog/ThereScope.swift
@@ -188,31 +188,3 @@ struct WavePlot: View {
         }
     }
 }
-
-//struct ThereScopeDevicePicker: View {
-//    @State var device: Device
-//    
-//    var body: some View {
-//        Picker("Input: \(device.deviceID)", selection: $device) {
-//            ForEach(getDevices(), id: \.self) {
-//                Text($0.deviceID)
-//            }
-//        }
-//        .pickerStyle(MenuPickerStyle())
-//        .foregroundColor(.black)
-//        .accentColor(.black)
-//        .onChange(of: device, perform: setInputDevice)
-//    }
-//    
-//    func getDevices() -> [Device] {
-//        AudioEngine.inputDevices.compactMap { $0 }
-//    }
-//    
-//    func setInputDevice(to device: Device) {
-//        do {
-//            try AudioEngine.setInputDevice(device)
-//        } catch let err {
-//            print(err)
-//        }
-//    }
-//}
