e314f2b19d5888df492a50cb49d7d6d73ec01ca8
Mike Crandall <michaelgcrandall@gmail.com>
2026-07-23 11:55:48 -0600

    added waveConductor

--- moog/TherescopeController.swift
+++ moog/TherescopeController.swift
@@ -6,22 +6,88 @@
 //
 
 import UIKit
+import SwiftUI
 
-class TherescopeController: UIViewController {
+// Bridges WaveConductor's published values into the existing WavePlot.
+private struct HostedWavePlot: View {
+    @ObservedObject var waveConductor: WaveConductor
     
-    @IBOutlet weak var titleLabel: UILabel!
+    var body: some View {
+        WavePlot(
+            waveData: waveConductor.waveData,
+            amplitudeScale: 0.8,
+            widthScale: 0.25,
+            minAmplitudeThreshold: 0.01,
+            minAmplitudeScale: 0.1,
+            minWidthScale: 0.5
+        )
+        .background(Color.black)
+        .clipped()
+    }
+}
 
+final class TherescopeController: UIViewController {
+    
+    @IBOutlet private weak var wavePlotContainerView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    private let waveConductor = WaveConductor()
+    
+    private var wavePlotHostingController:
+    UIHostingController<HostedWavePlot>?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        self.configureViews()
+        
+        embedWavePlot()
     }
     
-    func configureViews(){
-        print("configureViews")
-        titleLabel.text = "TherescopeController"
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        waveConductor.start()
     }
     
-
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        waveConductor.stop()
+    }
+    
+    private func embedWavePlot() {
+        let wavePlot = HostedWavePlot(
+            waveConductor: waveConductor
+        )
+        
+        let hostingController = UIHostingController(
+            rootView: wavePlot
+        )
+        
+        wavePlotHostingController = hostingController
+        
+        addChild(hostingController)
+        
+        let hostedView = hostingController.view!
+        hostedView.translatesAutoresizingMaskIntoConstraints = false
+        hostedView.backgroundColor = .black
+        
+        wavePlotContainerView.addSubview(hostedView)
+        
+        NSLayoutConstraint.activate([
+            hostedView.topAnchor.constraint(
+                equalTo: wavePlotContainerView.topAnchor
+            ),
+            hostedView.bottomAnchor.constraint(
+                equalTo: wavePlotContainerView.bottomAnchor
+            ),
+            hostedView.leadingAnchor.constraint(
+                equalTo: wavePlotContainerView.leadingAnchor
+            ),
+            hostedView.trailingAnchor.constraint(
+                equalTo: wavePlotContainerView.trailingAnchor
+            )
+        ])
+        
+        hostingController.didMove(toParent: self)
+    }
 }
